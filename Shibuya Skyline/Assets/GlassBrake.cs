add811df2c602f2dbf715d20a53b06fc4cee9fbf
Gwydionx <310137@gllm.ac.uk>
2019-06-26 00:38:11 +0100

    starting cutscnee
    starting cutscene

---
+++ Shibuya Skyline/Assets/GlassBrake.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlassBrake : MonoBehaviour
+{
+    public GameObject BrokenGlass;
+    public GameObject SolidGlass;
+    public GameObject Zero;
+    public GameObject Zero2;
+    public GameObject Player;
+    public GameObject FlyCam;
+
+    void Start()
+    {
+        StartCoroutine(Time());
+        
+    }
+    IEnumerator Time()
+    {
+        yield return new WaitForSecondsRealtime(4.8f);
+        Zero.SendMessage("Jump");
+        yield return new WaitForSecondsRealtime(0.8f);
+        SolidGlass.SetActive(false);
+        BrokenGlass.SetActive(true);
+        yield return new WaitForSecondsRealtime(1f);
+        Zero.SetActive(false);
+        Zero2.SetActive(true);
+        FlyCam.SetActive(true);
+        yield return new WaitForSecondsRealtime(3f);
+        Player.SetActive(true);
+        yield return new WaitForSecondsRealtime(1f);
+        FlyCam.SetActive(false);
+        Zero2.SetActive(false);
+
+
+
+    }
+}
---
+++ Shibuya Skyline/Assets/JumpScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpScript : MonoBehaviour
+{
+
+    public Animator animator;
+
+    public void Jump()
+    {
+        animator.SetBool("Jump", true);
+    }
+}
