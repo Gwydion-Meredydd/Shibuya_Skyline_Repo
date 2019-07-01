74a2cd1d25090f53df6c1225d5c24374e91e1ec9
Gwydionx <310137@gllm.ac.uk>
2019-07-01 18:23:05 +0100

    gg
    gg

--- Shibuya Skyline/Assets/Scripts/WayPoint_Manager.cs
+++ Shibuya Skyline/Assets/Scripts/WayPoint_Manager.cs
@@ -17,74 +17,94 @@ public class WayPoint_Manager : MonoBehaviour {
     public GameObject W_11;
     public GameObject W_12;
     public GameObject W_13;
+    public GameObject SoundEffect;
 
     public void Waypoint_1()
     {
         W_1.SetActive(false);
         W_2.SetActive(true);
+        StartCoroutine(Timer());
     }
     public void Waypoint_2()
     {
         W_2.SetActive(false);
         W_3.SetActive(true);
+        StartCoroutine(Timer());
     }
     public void Waypoint_3()
     {
         W_3.SetActive(false);
         W_4.SetActive(true);
+        StartCoroutine(Timer());
     }
     public void Waypoint_4()
     {
         W_4.SetActive(false);
         W_5.SetActive(true);
         W_10.SetActive(true);
+        StartCoroutine(Timer());
     }
     public void Waypoint_5()
     {
         W_5.SetActive(false);
         W_10.SetActive(false);
         W_6.SetActive(true);
+        StartCoroutine(Timer());
     }
     public void Waypoint_6()
     {
         W_6.SetActive(false);
         W_7.SetActive(true);
+        StartCoroutine(Timer());
     }
     public void Waypoint_7()
     {
         W_7.SetActive(false);
         W_8.SetActive(true);
+        StartCoroutine(Timer());
     }
     public void Waypoint_8()
     {
         W_8.SetActive(false);
         W_9.SetActive(true);
+        StartCoroutine(Timer());
     }
     public void Waypoint_9()
     {
         W_9.SetActive(false);
         W_10.SetActive(true);
+        StartCoroutine(Timer());
     }
     public void Waypoint_10()
     {
         W_5.SetActive(false);
         W_10.SetActive(false);
         W_11.SetActive(true);
+        StartCoroutine(Timer());
     }
     public void Waypoint_11()
     {
         W_11.SetActive(false);
         W_12.SetActive(true);
+        StartCoroutine(Timer());
     }
     public void Waypoint_12()
     {
         W_12.SetActive(false);
         W_13.SetActive(true);
+        StartCoroutine(Timer());
     }
     public void Waypoint_13()
     {
         W_13.SetActive(false);
-       // W_14.SetActive(true);
+        StartCoroutine(Timer());
+        // W_14.SetActive(true);
+    }
+    IEnumerator Timer()
+    {
+        SoundEffect.SetActive(true);
+        yield return new WaitForSecondsRealtime(1.5f);
+        SoundEffect.SetActive(false);
     }
 
 
